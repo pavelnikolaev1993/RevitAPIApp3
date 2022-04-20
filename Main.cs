a3ee463611c1659159fdc379d7c17435f0d91543
Павел Николаев <pavelnikolaev1993@mail.ru>
2022-04-20 12:45:35 +0300

    ргевневекнвнеквнекгвеквкевек

--- Main.cs
+++ Main.cs
@@ -1,5 +1,7 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -19,31 +21,72 @@ namespace RevitAPIFirstApp
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
-            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Face, "Выберите несколько стен по грани");
+            var categorySet = new CategorySet();
+            categorySet.Insert(Category.GetCategory(doc, BuiltInCategory.OST_PipeCurves));
 
-            var WallList = new List<Wall>();
-            double Value = 0;
-            double Sum = 0;
-
-            foreach (var selectedElement in selectedElementRefList)
+            using (Transaction ts = new Transaction(doc, "Add parametr"))
             {
-                Element element = doc.GetElement(selectedElement);
-
-                if (element is Wall)
+                ts.Start();
+                CreateSharedParameter(uiapp.Application, doc, "Наименование", categorySet, BuiltInParameterGroup.PG_DATA, true);
+                ts.Commit();
+            }
+            List<Pipe> pipes = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_PipeCurves)
+                .WhereElementIsNotElementType()
+                .Cast<Pipe>()
+                .ToList();
+            foreach (var selectedElement in pipes)
+            {
+                Element element = doc.GetElement(selectedElement.Id);
+                if (element is Pipe)
                 {
-                    Wall owall = (Wall)element;
-                    Parameter volumeParameter = element.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED);
-                    if (volumeParameter.StorageType == StorageType.Double)
+                    Parameter outerDiamParameter = element.get_Parameter(BuiltInParameter.RBS_PIPE_OUTER_DIAMETER);
+                    Parameter innerDiamParameter = element.get_Parameter(BuiltInParameter.RBS_PIPE_INNER_DIAM_PARAM);
+                    if (outerDiamParameter.StorageType == StorageType.Double && innerDiamParameter.StorageType == StorageType.Double)
                     {
-                        Value = UnitUtils.ConvertFromInternalUnits(volumeParameter.AsDouble(), UnitTypeId.CubicMeters);
-                        TaskDialog.Show("Объём i-ой стены ", Value.ToString());
+                        double outerDiamValue = UnitUtils.ConvertFromInternalUnits(outerDiamParameter.AsDouble(), UnitTypeId.Millimeters);
+                        double innerDiamValue = UnitUtils.ConvertFromInternalUnits(innerDiamParameter.AsDouble(), UnitTypeId.Millimeters);
+
+                        using (Transaction ts1 = new Transaction(doc, "Set parameter"))
+                        {
+                            ts1.Start();
+                            var pipe = element as Pipe;
+                            Parameter name = pipe.LookupParameter("Наименование");
+                            string output = $"{outerDiamValue}/{innerDiamParameter}";
+                            name.Set(output);
+                            ts1.Commit();
+                        }
                     }
-                    Sum += Value;
+
                 }
             }
-            TaskDialog.Show("Сумма объёмов стен ", Sum.ToString());
 
+            TaskDialog.Show("Успешно", message);
             return Result.Succeeded;
+        }
+
+        private void CreateSharedParameter(Application application, Document doc, string parameterName, CategorySet categorySet,
+            BuiltInParameterGroup builtInParameterGroup, bool isInstance)
+        {
+            DefinitionFile definitionFile = application.OpenSharedParameterFile();
+            if (definitionFile == null)
+            {
+                TaskDialog.Show("Ошибка", "не найден файл общих параметров");
+                return;
+            }
+            Definition definition = definitionFile.Groups.
+                SelectMany(group => group.Definitions).FirstOrDefault(def=>def.Name.Equals(parameterName));
+            if (definition == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найден указанный параметр");
+                return;
+            }
+            Binding binding = application.Create.NewTypeBinding(categorySet);
+            if (isInstance)
+                binding = application.Create.NewInstanceBinding(categorySet);
+
+            BindingMap map = doc.ParameterBindings;
+            map.Insert(definition, binding, builtInParameterGroup);
         }
     }
 }
